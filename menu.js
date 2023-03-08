bb1c3e7c444cf87ef0a2ee0ccc30563ce9880a89
Karl Remo <karlangelremo@gmail.com>
2023-03-08 08:12:20 -0500

    added menu toggle

--- menu.js
+++ menu.js
@@ -1,5 +1,22 @@
 const button = document.querySelector('.menu_button');
 console.log("test");
+var navBarToggled = false;
 button.addEventListener('click', () => {
-alert("Test");
-})
+    if (!navBarToggled) {
+        navBarToggled = true;
+    } else {
+        navBarToggled = false
+    }
+
+    if (!navBarToggled) {
+        console.log(`navbar is ${navBarToggled}`)
+        let element = document.getElementById('nav_bar_id');
+        console.log(element.style.paddingBottom);
+        element.style.paddingBottom = "0px"
+    } else {
+        console.log(`navbar is ${navBarToggled}`)
+        let element = document.getElementById('nav_bar_id');
+        console.log(element.style.paddingBottom)
+        element.style.paddingBottom = "100px"
+    }
+})                         
