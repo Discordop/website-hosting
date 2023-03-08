ceace634b65a4486f078782a43893775f6d1df57
Karl Remo <karlangelremo@gmail.com>
2023-03-08 12:24:18 -0500

    Basic Nav Bar Mobile

--- menu.js
+++ menu.js
@@ -10,13 +10,27 @@ button.addEventListener('click', () => {
 
     if (!navBarToggled) {
         console.log(`navbar is ${navBarToggled}`)
-        let element = document.getElementById('nav_bar_id');
-        console.log(element.style.paddingBottom);
-        element.style.paddingBottom = "0px"
+        let deleteElement = document.getElementsByClassName('mobile_div_nav');
+        deleteElement[0].remove();
+        let element = document.getElementsByClassName('navigation_bar');
+        element[0].style.borderBottom = "5px"
     } else {
         console.log(`navbar is ${navBarToggled}`)
-        let element = document.getElementById('nav_bar_id');
-        console.log(element.style.paddingBottom)
-        element.style.paddingBottom = "100px"
+        var navigationBar = document.getElementsByClassName('navigation_bar');
+        navigationBar[0].insertAdjacentHTML("afterend", `
+        <div class="mobile_div_nav">
+        <div class="break"></div>
+        <div class="mobile_div_nav">
+        <nav>
+        <ul class="nav_links">
+            <li><a href="./pages/services.html">Services</a></li>
+            <li><a href="#">Billing</a></li>
+            <li><a href="#">Game Pannel</a></li>
+            <li><a href="#">Server Status</a></li>
+        </ul>
+    </nav>
+    </div>`)
+        let element = document.getElementsByClassName('navigation_bar');
+        element[0].style.borderBottom = "1000px";
     }
 })                         
